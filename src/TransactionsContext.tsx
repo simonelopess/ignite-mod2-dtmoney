c2d2dc9fd7e3a026b25e2856709dadcd111fc245
DESKTOP-LF77V8L\simon <sisilopes20@gmail.com>
2021-09-19 17:09:53 -0300

    Move o método POST para dentro do context

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -10,10 +10,20 @@ interface Transaction {
   createdAt: string;
 }
 
+type TransactionInput = Omit<Transaction, "id" | "createdAt">;
+
 interface TransactionProviderProps {
   children: ReactNode;
 }
-export const TransactionContext = createContext<Transaction[]>([]);
+
+interface TransactionsContextData {
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => void;
+}
+
+export const TransactionContext = createContext<TransactionsContextData>(
+  {} as TransactionsContextData
+);
 
 export function TransactionProvider({ children }: TransactionProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -24,8 +34,11 @@ export function TransactionProvider({ children }: TransactionProviderProps) {
       .then((response) => setTransactions(response.data.transactions));
   }, []);
 
+  function createTransaction(transaction: TransactionInput) {
+    api.post("/transactions", transaction);
+  }
   return (
-    <TransactionContext.Provider value={transactions}>
+    <TransactionContext.Provider value={{ transactions, createTransaction }}>
       {children}
     </TransactionContext.Provider>
   );
